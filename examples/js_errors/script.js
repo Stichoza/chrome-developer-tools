a8ae711c898f728ab5e402286e6a1cc280765ab0
Stichoza <stichoza@gmail.com>
2013-12-21 09:19:41 +0400

    debugging examples

---
+++ examples/js_errors/script.js
@@ -0,0 +1,15 @@
+var list = document.getElementsByTagName("ul");
+var asd = null;
+
+try {
+	if (asd.length > 2) {
+		alert("yeah");
+	}
+} catch (err) {
+	console.log(err.message);
+}
+
+var d = new Date();
+d.wtfMethod();
+
+alert("Goodbye!");
---
+++ examples/js_errors/style.css
@@ -0,0 +1,20 @@
+body {
+	padding: 0;
+	margin: 0;
+}
+
+header {
+	padding: 50px;
+	background: #f00baa;
+	font-family: monospace;
+}
+
+header h1 {
+	color: #fff;
+	font-size: 3em;
+}
+
+p {
+	font-size: 2em;
+	color: #454545;
+}
